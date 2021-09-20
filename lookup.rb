407fd45fa3dd1a0697e782338dc29d325b6d47a7
meena <meenabhagavathula100@gmail.com>
2021-09-20 11:13:52 +0530

    updated code

--- lookup.rb
+++ lookup.rb
@@ -1,8 +1,4 @@
 def get_command_line_argument
-  # ARGV is an array that Ruby defines for us,
-  # which contains all the arguments we passed to it
-  # when invoking the script from the command line.
-  # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
   if ARGV.empty?
     puts "Usage: ruby lookup.rb <domain>"
     exit
@@ -10,52 +6,36 @@ def get_command_line_argument
   ARGV.first
 end
 
-# `domain` contains the domain name we have to look up.
 domain = get_command_line_argument
 
-# File.readlines reads a file and returns an
-# array of string, where each element is a line
-# https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
 
-
-# parse_dns to return dns as records - hash
-def parse_dns(dnsstring)
-  #remove empty lines
-  dnsstring=dnsstring.reject { |s| s.strip.empty? }
-  # removing first hash line
-  dnsstring=dnsstring[1..-1]
-  # creating empty hash
-  dnsreturn={}
-  for i in 0..dnsstring.length-1 do
-    temp=dnsstring[i].split(",")
-    temphash= Hash.new
-    temphash[:type]=temp[0].strip
-    temphash[:target]=temp[2].strip
-    dnsreturn[temp[1].strip]=temphash
+def parse_dns(raw)
+  dnshash=Hash.new
+  raw=raw.reject { |line| line.start_with?("#")}
+  raw=raw.reject { |line| line.empty? }
+  #raw=raw.map { |line| line.strip.split(", ")}
+  raw.each_with_object({}) do |record,records|
+    record=record.strip.split(", ")
+    records[:type]=record[0]
+    records[:target]=record[2]
+    dnshash[record[1]]=records
   end
-  return(dnsreturn)
+  return dnshash
 end
 
-# dns resolver
 def resolve(dns_records,lookup_chain,domain)
   rec=dns_records[domain]
-  if !(rec)
-    lookup_chain=[]
-    errorMessage = "Error: record not found for "+domain.to_s
-    lookup_chain.push(errorMessage)
+  if(!rec)
+    lookup_chain<<"Error: record not found for #{domain}"
   elsif rec[:type]=="A"
-    lookup_chain.push(rec[:target])
+    lookup_chain << rec[:target]
   elsif rec[:type]=="CNAME"
-    lookup_chain.push(rec[:target])
+    #lookup_chain << rec[:target]
     resolve(dns_records,lookup_chain,rec[:target])
-
   end
 end
-
-
 dns_records = parse_dns(dns_raw)
 lookup_chain = [domain]
-
 lookup_chain = resolve(dns_records, lookup_chain, domain)
 puts lookup_chain.join(" => ")
